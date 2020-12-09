04b88fc50f63278e914713580447cf00e20bb43a
BeaLove <beatriceelovely@gmail.com>
2020-12-09 19:02:14 +0100

    convergence stops...

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class VI():
         self.a_0 = 0
         self.b_0 = 0
         self.lambda_0 = 0
+        ##lambda 10.27 from bishop
+        self.lambda_n = 0
         self.expected_mu = 0
         self.expected_mu_2nd = 0
         self.expected_tau = 0
@@ -20,6 +22,13 @@ class VI():
         self.alpha = 0
         self.n = 0
         self.data = data
+        N = len(data)
+        sample_mean = np.mean(data)
+        var = np.sum((data - sample_mean) ** 2)
+        self.actual_a = 0.5*N
+        self.actual_b = 0.5*var
+        self.actual_tau = self.actual_a/self.actual_b
+        self.actual_lambda = N
 
     def initialize(self, tau):
         self.n = len(self.data)
@@ -41,6 +50,7 @@ class VI():
                     self.expected_mu_2nd - self.mu_0 ** 2 - 2 * self.expected_mu * self.mu_0) + 0.5 * sum
         # beta = 0.5*sum
         self.expected_tau = self.alpha / beta
+        self.lambda_n = (self.lambda_0+self.n)*self.expected_tau
         return beta
 
     def update_mu_elbo(self, beta):
@@ -55,17 +65,15 @@ class VI():
         diff_beta = 1000
         old_beta = 0
         print(self.actual_mu, self.actual_variance)
-        while diff_tau and diff_beta > 10e-3:
+        while diff_tau and diff_beta > 10e-5:
             new_beta = self.update_tau_elbo(old_tau)
             new_tau = self.update_mu_elbo(new_beta)
             print("beta", new_beta)
             print("1/tau", 1 / new_tau)
             print("1/expected tau", 1 / self.expected_tau)
-            self.plot_normal_gamma(new_tau, new_beta)
-            plt.scatter(self.data, norm.pdf(self.data, self.actual_mu, self.actual_variance))
+            plot_prior(self.actual_mu, self.actual_tau, self.actual_lambda, self.actual_a, self.actual_b, self.data)
             plt.subplot()
-            plt.scatter(self.data, norm.pdf(self.data, self.mu, 1 / new_tau))
-
+            plot_prior(self.mu, new_tau, self.lambda_n, self.alpha, new_beta, self.data, color='green')
             plt.show()
             diff_tau = abs(new_tau - old_tau)
             diff_beta = abs(new_beta - old_beta)
@@ -88,14 +96,8 @@ class VI():
         plt.contour(mu_plot, tau_plot, pdf_array)
         plt.show()
 
-    def normal_gamma_pdf(self, x, y, tau, beta):
-        lambda_ = len(self.data)  ##??
-        ## WHAT IS LAMBDA??
-        ###alt:
-        lambda_ = tau  ##??
-        gamma_alpha = math.gamma(alpha)
-        return ((beta ** self.alpha * math.sqrt(lambda_)) / (gamma_alpha * math.sqrt(2 * math.pi))) * (
-                    y ** (self.alpha / 2)) * math.exp(-beta * y) * math.exp((lambda_ * y * (x - self.mu) ** 2) / 2)
+    #def plot_inferred_posterior(self):
+
 
 
 def compute_normal_prob(x, mean, precision):
@@ -107,28 +109,23 @@ def compute_gamma_prob(tau, a, b):
     return dist
 
 
-def plot_true_prior(true_mu, true_variance, data):
+def plot_prior(mu, tau, lambda_, a_n, b_n, data, color='blue'):
     #true_tau = 1 / true_variance
     #true_tau = true_variance
     N = len(data)
-    mu_range = np.linspace(true_mu-1, true_mu + 1, 100)
-    tau_range = np.linspace(true_variance - 1, true_variance + 1.5, 100)
+    mu_range = np.linspace(-1, 1, 100)
+    tau_range = np.linspace(0, 2, 100)
     mu_grid, tau_grid = np.meshgrid(mu_range, tau_range, indexing='ij')
     z = np.zeros_like(mu_grid)
-    sample_mean = np.mean(data)
-    var = np.sum((data-sample_mean)**2)
-    lambda_ = N
-    a_n = N/2
-    b_n = 0.5*var
-    true_tau = a_n/b_n
-    for i, mu in enumerate(mu_range):
-        for j, tau in enumerate(tau_range):
+
+    for i, mu_ in enumerate(mu_range):
+        for j, tau_ in enumerate(tau_range):
             #z[i, j] = normal_gamma(data, mu, tau)
-            z[i, j] = compute_normal_prob(mu, true_mu, lambda_*true_tau)*compute_gamma_prob(tau, a_n, b_n)
-    plt.contour(mu_grid, tau_grid, z)
+            z[i, j] = compute_normal_prob(mu_, mu, lambda_*tau)*compute_gamma_prob(tau_, a_n, b_n)
+    plt.contour(mu_grid, tau_grid, z, color=color)
     plt.xlabel("Mean")
     plt.ylabel("Precision")
-    plt.show()
+    #plt.show()
 
 
 def normal_gamma(data, my_mu, my_tau):
@@ -159,15 +156,15 @@ np.random.seed(50)
 x = np.random.normal(loc=mu, scale=sigma, size=sample)
 #p_mu = norm(mu, sigma).pdf(x)
 #p_tau = gamma.pdf(x, 0)
-plot_true_prior(mu, sigma, x)
+#plot_prior(mu, sigma, x)
 # z = norm.pdf(*np.meshgrid(mu_plot, tau_plot), mu_plot, tau_plot)
 # plt.contour(mu_plot, tau_plot, z)
 # plt.show()
 
 # plt.scatter(x, norm.pdf(x, mu, sigma))
 # plt.show()
-# vi = VI(x, mu, sigma)
-# start_tau = np.random.uniform(2,3)
-start_tau = 0.4
+vi = VI(x, mu, sigma)
+start_tau = np.random.uniform(0,1)
+#start_tau = 0.4
 print('start tau', start_tau)
-# vi.fit(start_tau)
+vi.fit(start_tau)
